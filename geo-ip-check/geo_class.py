0d181e517422b7fb0a959f6492f8af2001a56b8b
rumblefish <alexnvis@gmail.com>
2021-01-31 01:47:19 +0100

    maxmind and ripe added + nocache option

--- geo-ip-check/geo_class.py
+++ geo-ip-check/geo_class.py
@@ -1,12 +1,14 @@
 import requests
 import os
 import json
+from ipwhois import IPWhois
 
 class geo_data:
-    def __init__(self, ip_address, service, apiKey):
+    def __init__(self, ip_address, service, apiKey, cache):
         self.ip_address = ip_address
         self.apiKey = apiKey
         self.service = service
+        self.cache=cache
 
     '''
     Method is ip_check() is used to validate the IP address
@@ -114,86 +116,161 @@ class geo_data:
     def get_info(self) :
         self.ip_check()
         net_ip_address = self.find_net()
-        try:
-            directory = './cache/'
-            filename = self.service + '_' + net_ip_address + '.json'
-            file_path = os.path.join(directory, filename)
 
-            with open(file_path, 'r') as f:
-                ip_json=f.read()
+        if self.cache != 'nocache':
+            try:
+                directory = './cache/'
+                filename = self.service + '_' + net_ip_address + '.json'
+                file_path = os.path.join(directory, filename)
 
-            ip_addr = json.loads(ip_json)
+                with open(file_path, 'r') as f:
+                    ip_json=f.read()
 
-            if self.service == 'ip2location' :
-                geo_info = {'database':'ip2location.com',
-                            'code': ip_addr["country_code"],
-                            'country': ip_addr["country_name"],
-                            'region' : ip_addr["region_name"],
-                            'city': ip_addr["city_name"],
-                            'company': '',
-                            'isp': ip_addr["isp"],
-                            'latitude' : ip_addr["latitude"],
-                            'longitude' : ip_addr["longitude"]}
+                ip_addr = json.loads(ip_json)
 
-            elif self.service == 'ipgeolocation':
-                geo_info = {'database': 'ipgeolocation.io',
-                            'code': ip_addr["country_code2"],
-                            'country': ip_addr["country_name"],
-                            'region': ip_addr["state_prov"],
-                            'city': ip_addr["city"],
-                            'company': ip_addr["organization"],
-                            'isp': ip_addr["isp"],
-                            'latitude': ip_addr["latitude"],
-                            'longitude': ip_addr["longitude"]}
+                if self.service == 'ip2location' :
+                    geo_info = {'database':'ip2location.com',
+                                'code': ip_addr["country_code"],
+                                'country': ip_addr["country_name"],
+                                'region' : ip_addr["region_name"],
+                                'city': ip_addr["city_name"],
+                                'company': '',
+                                'isp': ip_addr["isp"],
+                                'latitude' : ip_addr["latitude"],
+                                'longitude' : ip_addr["longitude"]}
 
-            elif self.service == 'ipregistry':
-                geo_info = {'database': 'ipregistry.co',
-                            'code': ip_addr["location"]["country"]["code"],
-                            'country': ip_addr["location"]["country"]["name"],
-                            'region': ip_addr["location"]["region"]["name"],
-                            'city': ip_addr["location"]["city"],
-                            'company': '',
-                            'isp': ip_addr["connection"]["organization"],
-                            'latitude': ip_addr["location"]["latitude"],
-                            'longitude': ip_addr["location"]["longitude"]}
+                elif self.service == 'ipgeolocation':
+                    geo_info = {'database': 'ipgeolocation.io',
+                                'code': ip_addr["country_code2"],
+                                'country': ip_addr["country_name"],
+                                'region': ip_addr["state_prov"],
+                                'city': ip_addr["city"],
+                                'company': ip_addr["organization"],
+                                'isp': ip_addr["isp"],
+                                'latitude': ip_addr["latitude"],
+                                'longitude': ip_addr["longitude"]}
 
-            elif self.service == 'abstractapi':
-                geo_info = {'database': 'abstractapi.com',
-                            'code': ip_addr["country_code"],
-                            'country': ip_addr["country"],
-                            'region': ip_addr["region"],
-                            'city': ip_addr["city"],
-                            'company': ip_addr["connection"]["organization_name"],
-                            'isp': ip_addr["connection"]["isp_name"],
-                            'latitude': ip_addr["latitude"],
-                            'longitude': ip_addr["longitude"]}
+                elif self.service == 'ipregistry':
+                    geo_info = {'database': 'ipregistry.co',
+                                'code': ip_addr["location"]["country"]["code"],
+                                'country': ip_addr["location"]["country"]["name"],
+                                'region': ip_addr["location"]["region"]["name"],
+                                'city': ip_addr["location"]["city"],
+                                'company': '',
+                                'isp': ip_addr["connection"]["organization"],
+                                'latitude': ip_addr["location"]["latitude"],
+                                'longitude': ip_addr["location"]["longitude"]}
 
-            elif self.service == 'ipdata':
-                geo_info = {'database': 'ipdata.co',
-                            'code': ip_addr["country_code"],
-                            'country': ip_addr["country_name"],
-                            'region': ip_addr["region"],
-                            'city': ip_addr["city"],
-                            'company': '',
-                            'isp': ip_addr["asn"]["name"],
-                            'latitude': ip_addr["latitude"],
-                            'longitude': ip_addr["longitude"]}
+                elif self.service == 'abstractapi':
+                    geo_info = {'database': 'abstractapi.com',
+                                'code': ip_addr["country_code"],
+                                'country': ip_addr["country"],
+                                'region': ip_addr["region"],
+                                'city': ip_addr["city"],
+                                'company': ip_addr["connection"]["organization_name"],
+                                'isp': ip_addr["connection"]["isp_name"],
+                                'latitude': ip_addr["latitude"],
+                                'longitude': ip_addr["longitude"]}
 
-            elif self.service == 'astroip':
-                geo_info = {'database': 'astroip.co',
-                            'code': ip_addr["geo"]["country_code"],
-                            'country': ip_addr["geo"]["country_name"],
-                            'region': ip_addr["geo"]["region_name"],
-                            'city': ip_addr["geo"]["city"],
-                            'company': ip_addr["asn"]["organization"],
-                            'isp': ip_addr["asn"]["asn"],
-                            'latitude': ip_addr["geo"]["latitude"],
-                            'longitude': ip_addr["geo"]["longitude"]}
+                elif self.service == 'ipdata':
+                    geo_info = {'database': 'ipdata.co',
+                                'code': ip_addr["country_code"],
+                                'country': ip_addr["country_name"],
+                                'region': ip_addr["region"],
+                                'city': ip_addr["city"],
+                                'company': '',
+                                'isp': ip_addr["asn"]["name"] if ("asn" in ip_addr) else '',
+                                'latitude': ip_addr["latitude"] if ("latitude" in ip_addr) else '',
+                                'longitude': ip_addr["longitude"] if ("longitude" in ip_addr) else ''
+                                }
+
+                elif self.service == 'astroip':
+                    geo_info = {'database': 'astroip.co',
+                                'code': ip_addr["geo"]["country_code"],
+                                'country': ip_addr["geo"]["country_name"],
+                                'region': ip_addr["geo"]["region_name"],
+                                'city': ip_addr["geo"]["city"],
+                                'company': ip_addr["asn"]["organization"] if (ip_addr["asn"] is not None) else '',
+                                'isp': ip_addr["asn"]["asn"] if (ip_addr["asn"] is not None) else '',
+                                'latitude': ip_addr["geo"]["latitude"],
+                                'longitude': ip_addr["geo"]["longitude"]}
 
 
-            return (geo_info)
+                return (geo_info)
 
-        except IOError:
+            except IOError:
+                ip_addr = self.connect_api()
+                if self.service == 'ip2location':
+                    geo_info = {'database': 'ip2location.com',
+                                'code': ip_addr["country_code"],
+                                'country': ip_addr["country_name"],
+                                'region': ip_addr["region_name"],
+                                'city': ip_addr["city_name"],
+                                'company': '',
+                                'isp': ip_addr["isp"],
+                                'latitude': ip_addr["latitude"],
+                                'longitude': ip_addr["longitude"]}
+
+                elif self.service == 'ipgeolocation':
+                    geo_info = {'database': 'ipgeolocation.io',
+                                'code': ip_addr["country_code2"],
+                                'country': ip_addr["country_name"],
+                                'region': ip_addr["state_prov"],
+                                'city': ip_addr["city"],
+                                'company': ip_addr["organization"],
+                                'isp': ip_addr["isp"],
+                                'latitude': ip_addr["latitude"],
+                                'longitude': ip_addr["longitude"]}
+
+                elif self.service == 'ipregistry':
+                    geo_info = {'database': 'ipregistry.co',
+                                'code': ip_addr["location"]["country"]["code"],
+                                'country': ip_addr["location"]["country"]["name"],
+                                'region': ip_addr["location"]["region"]["name"],
+                                'city': ip_addr["location"]["city"],
+                                'company': '',
+                                'isp': ip_addr["connection"]["organization"],
+                                'latitude': ip_addr["location"]["latitude"],
+                                'longitude': ip_addr["location"]["longitude"]}
+
+                elif self.service == 'abstractapi':
+                    geo_info = {'database': 'abstractapi.com',
+                                'code': ip_addr["country_code"],
+                                'country': ip_addr["country"],
+                                'region': ip_addr["region"],
+                                'city': ip_addr["city"],
+                                'company': ip_addr["connection"]["organization_name"],
+                                'isp': ip_addr["connection"]["isp_name"],
+                                'latitude': ip_addr["latitude"],
+                                'longitude': ip_addr["longitude"]}
+
+                elif self.service == 'ipdata':
+                    geo_info = {'database': 'ipdata.co',
+                                'code': ip_addr["country_code"],
+                                'country': ip_addr["country_name"],
+                                'region': ip_addr["region"],
+                                'city': ip_addr["city"],
+                                'company': '',
+                                'isp': ip_addr["asn"]["name"] if ("asn" in ip_addr) else '',
+                                'latitude': ip_addr["latitude"] if ("latitude" in ip_addr) else '',
+                                'longitude': ip_addr["longitude"] if ("longitude" in ip_addr) else ''}
+
+                elif self.service == 'astroip':
+                    geo_info = {'database': 'astroip.co',
+                                'code': ip_addr["geo"]["country_code"],
+                                'country': ip_addr["geo"]["country_name"],
+                                'region': ip_addr["geo"]["region_name"],
+                                'city': ip_addr["geo"]["city"],
+                                'company': ip_addr["asn"]["organization"] if (ip_addr["asn"] is not None) else '',
+                                'isp': ip_addr["asn"]["asn"] if (ip_addr["asn"] is not None) else '',
+                                'latitude': ip_addr["geo"]["latitude"],
+                                'longitude': ip_addr["geo"]["longitude"]}
+
+                #        print(json.dumps(ip_addr, indent=2))
+
+                return geo_info
+
+        else:
             ip_addr = self.connect_api()
             if self.service == 'ip2location':
                 geo_info = {'database': 'ip2location.com',
@@ -246,9 +323,9 @@ class geo_data:
                             'region': ip_addr["region"],
                             'city': ip_addr["city"],
                             'company': '',
-                            'isp': ip_addr["asn"]["name"],
-                            'latitude': ip_addr["latitude"],
-                            'longitude': ip_addr["longitude"]}
+                            'isp': ip_addr["asn"]["name"] if ("asn" in ip_addr) else '',
+                            'latitude': ip_addr["latitude"] if ("latitude" in ip_addr) else '',
+                            'longitude': ip_addr["longitude"] if ("longitude" in ip_addr) else ''}
 
             elif self.service == 'astroip':
                 geo_info = {'database': 'astroip.co',
@@ -256,11 +333,64 @@ class geo_data:
                             'country': ip_addr["geo"]["country_name"],
                             'region': ip_addr["geo"]["region_name"],
                             'city': ip_addr["geo"]["city"],
-                            'company': ip_addr["asn"]["organization"],
-                            'isp': ip_addr["asn"]["asn"],
+                            'company': ip_addr["asn"]["organization"] if (ip_addr["asn"] is not None) else '',
+                            'isp': ip_addr["asn"]["asn"] if (ip_addr["asn"] is not None) else '',
                             'latitude': ip_addr["geo"]["latitude"],
                             'longitude': ip_addr["geo"]["longitude"]}
 
             #        print(json.dumps(ip_addr, indent=2))
 
-            return (geo_info)
+            return geo_info
+
+class rir_data:
+    def __init__(self, ip_address):
+        self.ip_address = ip_address
+    def ip_check(self):
+        ip_address = self.ip_address
+        ip = ip_address.split(".")
+
+        if (ip.__len__()) != 4:
+            print("You haven't entered the valid IP address with a '.' notation")
+            self.ip_address = input("Please enter a valid IP address : ")
+            self.ip_check()
+        else:
+            try:
+                for i in ip:
+                    if (int(i) < 0) or (int(i) > 255):
+                        self.ip_address = input("Please enter valid IP address : ")
+                        self.ip_check()
+                        break
+            except:
+                print("Sorry, IP address has 4 fields with values 0-255 separated by comma, ")
+                self.ip_address = input("please enter a valid IP address : ")
+                self.ip_check()
+        return (ip_address)
+
+
+    def get_info(self):
+        self.ip_check()
+        obj = IPWhois(self.ip_address)
+        ip_rir = obj.lookup_rdap(depth=2)
+
+        entities = ip_rir["entities"]
+        #print(entities)
+        org_name = ''
+        for i in entities :
+            if i[0:3] == "ORG":
+                org_name = i
+
+        rir_info = { 'network' : ip_rir["network"]["cidr"],
+                    'parent_network' : ip_rir["network"]["parent_handle"],
+                    'database': ip_rir["asn_registry"],
+                    'network_name': ip_rir["network"]["name"],
+                    'code': ip_rir["network"]["country"],
+                    'type': ip_rir["network"]["type"],
+                    'org': org_name if (org_name != '') else '',
+                    'company': ip_rir["objects"][org_name]["contact"]["name"] if (org_name != '') else '',
+                    'address': ip_rir["objects"][org_name]["contact"]["address"][0]["value"] if (org_name!='') else '',
+                    'asn' : ip_rir["asn"],
+                    'asn_country': ip_rir["asn_country_code"]
+         }
+
+        return rir_info
+
--- geo-ip-check/geo_ip.py
+++ geo-ip-check/geo_ip.py
@@ -1,6 +1,7 @@
 from sys import argv
 import time
 import geo_class as ip
+#need to run pip install geoip2 to install maxmind module and the make import geoip2.webservice
 
 start_time = time.time()
 data = []
@@ -12,13 +13,24 @@ ipdata_apiKey = ""
 astroip_apiKey = ""
 
 ip_address = argv[1]
+cache = 'yes'
+try :
+    cache = argv[2]
+except:
+    pass
 
-ip2location = ip.geo_data(ip_address, 'ip2location', ip2location_apiKey)
-ipgeolocation = ip.geo_data(ip_address, 'ipgeolocation', ipgeolocation_apiKey)
-ipregistry = ip.geo_data(ip_address, 'ipregistry', ipregistry_apiKey)
-abstractapi = ip.geo_data(ip_address, 'abstractapi', abstractapi_apiKey)
-ipdata = ip.geo_data(ip_address, 'ipdata', ipdata_apiKey)
-astroip = ip.geo_data(ip_address, 'astroip', astroip_apiKey)
+rir_location = ip.rir_data(ip_address)
+rir_data = rir_location.get_info()
+
+maxmind_webservice =  geoip2.webservice.Client(XXXX, '')  #XXXX - your maxmind ID, and then will come  maxmind webservice key as a second argument
+maxmind_data = maxmind_webservice.insights(ip_address)
+
+ip2location = ip.geo_data(ip_address, 'ip2location', ip2location_apiKey, cache)
+ipgeolocation = ip.geo_data(ip_address, 'ipgeolocation', ipgeolocation_apiKey, cache)
+ipregistry = ip.geo_data(ip_address, 'ipregistry', ipregistry_apiKey, cache)
+abstractapi = ip.geo_data(ip_address, 'abstractapi', abstractapi_apiKey, cache)
+ipdata = ip.geo_data(ip_address, 'ipdata', ipdata_apiKey, cache)
+astroip = ip.geo_data(ip_address, 'astroip', astroip_apiKey, cache)
 
 
 data.append(ip2location.get_info())
@@ -27,6 +39,38 @@ data.append(ipregistry.get_info())
 data.append(abstractapi.get_info())
 data.append(ipdata.get_info())
 data.append(astroip.get_info())
+
+
+print(f'''
+------ {rir_data["database"].capitalize()} ------
+IP address :  {rir_location.ip_address}
+Network :  {rir_data["network"]}
+Type: {rir_data["type"]}
+Parent network : {rir_data["parent_network"]}
+Country code : {rir_data["code"]}
+Org object :  {rir_data["org"]}
+Company : {rir_data["company"]}
+Address : {rir_data["address"]}
+ASN : AS{rir_data["asn"]}
+ASN Country : {rir_data["asn_country"]}
+'''
+)
+
+print(f'''
+------ Maxmind Webservice ------
+IP address :  {maxmind_data.traits.ip_address}
+Network :  {maxmind_data.traits._network}
+Country code : {maxmind_data.country.iso_code}
+Country : {maxmind_data.country.name}
+Region: {maxmind_data.subdivisions[0].names["en"]}
+City: {maxmind_data.subdivisions[1].names["en"]}
+Postal code: {maxmind_data.postal.code}
+Company : {maxmind_data.traits.organization}
+ISP: {maxmind_data.traits.isp}
+Geolocation: {maxmind_data.location.latitude}, {maxmind_data.location.longitude}
+'''
+)
+
 
 for i in range(6):
     print(f'''
